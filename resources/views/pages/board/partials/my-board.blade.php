59b3941b7e4177d1b90bca9ab871a85ecc621d6f
antaress <nanatchendopjoelivan@gmail.com>
2026-02-08 00:52:02 +0100

    feat: implement tab to show personal board or collab board

---
+++ resources/views/pages/board/partials/my-board.blade.php
@@ -0,0 +1,22 @@
+<div class="space-y-4 sm:space-y-6 md:space-y-8">
+    @if ($this->boards->isNotEmpty())
+        <div>
+            @foreach ($this->boards as $board)
+                <livewire:board-item :wire:key="$board->id" :board="$board" @board-deleted="$refresh" lazy />
+            @endforeach
+        </div>
+        <div>
+            {{ $this->boards->links() }}
+        </div>
+    @else
+        <!-- Empty state -->
+        <div class="text-center py-10">
+            <div class="p-3.5 mx-auto mb-4 md:mb-6 border border-neutral-200 rounded-xl grid place-items-center shadow-xl w-max">
+                <x-lucide-folder class="size-6" />
+            </div>
+            <h3 class="font-medium">Votre espace de travail est vide</h3>
+            <p class="text-neutral-500 mt-1 text-sm text-pretty">Commencez dès maintenant en créant un projet et transformez vos <br class="hidden sm:block"/> idées en actions concrètes.</p>
+            <x-ui.button size="md" class="mt-6 font-medium">Commercer un projet</x-ui.button>
+        </div>
+    @endif
+</div>
