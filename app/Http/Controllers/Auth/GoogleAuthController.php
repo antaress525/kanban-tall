b9159aa3f5fa80a7e9b38ff0cd60e93bb129a9ec
antaress <nanatchendopjoelivan@gmail.com>
2026-02-09 20:24:04 +0100

    feat: integrate Google authentication using Laravel Socialite

---
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')
+            ->stateless()
+            ->redirect();
+    }
+
+    public function callback()
+    {
+        $googleUser = Socialite::driver('google')
+            ->stateless()
+            ->user();
+
+        $user = User::where('email', $googleUser->email)->first();
+
+        if ($user) {
+            if (!$user->google_id) {
+                $user->update([
+                    'google_id' => $googleUser->id,
+                    'avatar' => $googleUser->avatar,
+                ]);
+            }
+        } else {
+            $user = User::create([
+                'name' => $googleUser->name,
+                'email' => $googleUser->email,
+                'google_id' => $googleUser->id,
+                'avatar' => $googleUser->avatar,
+                'password' => bcrypt(Str::random(24)),
+                'email_verified_at' => now(),
+            ]);
+        }
+
+        Auth::login($user);
+
+        return redirect()->route('dashboard');
+    }
+}
