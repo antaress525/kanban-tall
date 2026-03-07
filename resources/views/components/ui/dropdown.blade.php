27b92da3e7b8420a5bdd63e1c7b925281ba764eb
antaress <nanatchendopjoelivan@gmail.com>
2026-03-07 23:28:58 +0100

    feat: implement member list dropdown for board with avatar display and toggle functionality

--- resources/views/components/ui/dropdown.blade.php
+++ resources/views/components/ui/dropdown.blade.php
@@ -4,7 +4,7 @@
 
 <div x-data="{ open: false }" class="relative">
     <!-- Trigger -->
-    <div @click="open = !open" @click.away="open = false">
+    <div @click="open = !open" @click.away="open = false" class="cursor-pointer">
         {{ $trigger }}
     </div>
 
@@ -12,7 +12,7 @@
     <div
         x-show="open"
         x-transition
-        class="absolute p-1 z-50 bg-white border border-neutral-200 rounded-md shadow-lg mt-2 min-w-40"
+        class="absolute p-1 z-50 bg-white border border-neutral-200 rounded-md shadow-lg mt-2 min-w-64"
         :class="{
             'left-0': '{{ $position }}'.includes('left'),
             'right-0': '{{ $position }}'.includes('right'),
